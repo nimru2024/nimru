ba4783fdea57f1b052bcfb5cb6a1ef712fca3ef6
nimru2024 <131301541+nimru2024@users.noreply.github.com>
2023-04-19 23:34:50 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 设置各变量
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
+UUID='7304c369-7eb6-4526-89ad-cedea8d6012e'
 VMESS_WSPATH='/vmess'
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
